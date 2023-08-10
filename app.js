3dc9fb36e4436ea3436064a986ed679ddfc435d8
ShivaGanji57 <shivakumarganjiatme@gmail.com>
2023-08-10 17:57:17 +0530

    added indexx.html and DOM

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("I love Git")
+console.log(document.URL)
+console.log(document.title)
+console.log(document.doctype)
+console.log(document.head)
+console.log(document.body)
+console.log(document.all)
+console.log(document.forms)
+
+var items=document.getElementsByClassName('list-group-item')
+console.log(items)
+items[1].textContent='Shiva';
+items[1].style.fontWeight='bold'
+items[1].style.color='green'
+items[1].style.backgroundColor='pink'
