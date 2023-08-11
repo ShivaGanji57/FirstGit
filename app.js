6cf7bcc08b84a852bb53eed743f0784d100f8c7e
ShivaGanji57 <shivakumarganjiatme@gmail.com>
2023-08-11 12:48:17 +0530

    added indexx.html and DOM

--- app.js
+++ app.js
@@ -12,7 +12,3 @@ items[1].textContent='Shiva';
 items[1].style.fontWeight='bold'
 items[1].style.color='green'
 items[1].style.backgroundColor='pink'
-items[2].style.backgroundColor='green'
-for(let i=0;i<items.length;i++){
-    items[i].style.fontWeight='bold'
-}
