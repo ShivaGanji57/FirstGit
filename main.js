ec7963424beb31a538506ce1629684772f46c0dd
ShivaGanji57 <shivakumarganjiatme@gmail.com>
2023-08-12 10:05:49 +0530

    assignment 8

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let form=document.getElementById('addForm')
 var itemList=document.getElementById('items')
 //form submit event
 form.addEventListener('submit',addItem);
-
+itemList.addEventListener('click',removeItem)
 //addItem function
 function addItem(e){
     e.preventDefault()
@@ -14,10 +14,27 @@ function addItem(e){
     console.log(li)
 
     li.appendChild(document.createTextNode(newItem))
-
+    //add delete button
     var deletebtn=document.createElement('button')
     deletebtn.className='btn btn-danger btn-sm float-right delete'
     deletebtn.appendChild(document.createTextNode('X'))
     li.appendChild(deletebtn)
+
+    //add edit button
+    var editbtn=document.createElement('button')
+    editbtn.className='btn btn-info btn-sm float-right edit'
+    editbtn.appendChild(document.createTextNode('Edit'))
+    li.appendChild(editbtn)
     itemList.appendChild(li)
 }
+
+// remove item from list
+function removeItem(e){
+    if(e.target.classList.contains('delete')){
+        if(confirm('Are you sure ?')){
+            var li=e.target.parentElement;
+            itemList.removeChild(li)
+        }
+    }
+}
+
